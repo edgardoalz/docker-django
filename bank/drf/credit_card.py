70523e20af95fe51c43a21533c8f495c0e975060
Jose Solano <jesolano@protonmail.com>
2024-07-28 13:48:37 -0700

    Add credit card rest services

--- bank/drf/credit_card.py
+++ bank/drf/credit_card.py
@@ -5,50 +5,57 @@ from account.models.account import Account
 from multitenant.drf.fields import CurrentTenantDefault
 from multitenant.drf.viewsets import TenantModelViewSet
 
-from ..models import BankAccount
+from ..models import CreditCard
 from ..models.bank import Bank
 from .bank import BankSerializer
 
 
-class BankAccountSerializer(serializers.ModelSerializer[BankAccount]):
+class CreditCardSerializer(serializers.ModelSerializer[CreditCard]):
     account = AccountSerializer()
     bank = BankSerializer()
 
     class Meta:
-        model = BankAccount
+        model = CreditCard
         fields = [
             "uuid",
             "account_number",
             "name",
             "owner_name",
-            "type",
+            "closing_day",
+            "payment_due_day",
+            "credit_limit",
             "bank",
             "account",
         ]
         read_only_fields = ["uuid", "account", "bank"]
 
 
-class UpdateBankAccountSerializer(serializers.ModelSerializer[BankAccount]):
+class UpdateCreditCardSerializer(serializers.ModelSerializer[CreditCard]):
     bank = serializers.SlugRelatedField(
         slug_field="uuid",
         queryset=Bank.objects.all(),
     )
 
     class Meta:
-        model = BankAccount
+        model = CreditCard
         fields = [
             "account_number",
             "name",
             "owner_name",
-            "type",
+            "closing_day",
+            "payment_due_day",
+            "credit_limit",
             "bank",
         ]
 
-    def to_representation(self, instance: BankAccount) -> dict:
-        return BankAccountSerializer(instance).data
+    def update(self, instance: CreditCard, validated_data) -> CreditCard:
+        return super().update(instance, validated_data)
+
+    def to_representation(self, instance: CreditCard) -> dict:
+        return CreditCardSerializer(instance).data
 
 
-class CreateBankAccountSerializer(serializers.ModelSerializer[BankAccount]):
+class CreateCreditCardSerializer(serializers.ModelSerializer[CreditCard]):
     initial_balance = AccountSerializer().get_fields()["initial_balance"]
     bank = serializers.SlugRelatedField(
         slug_field="uuid",
@@ -57,34 +64,36 @@ class CreateBankAccountSerializer(serializers.ModelSerializer[BankAccount]):
     tenant = serializers.HiddenField(default=CurrentTenantDefault())
 
     class Meta:
-        model = BankAccount
+        model = CreditCard
         fields = [
             "account_number",
             "name",
             "owner_name",
-            "type",
             "initial_balance",
+            "closing_day",
+            "payment_due_day",
+            "credit_limit",
             "bank",
             "tenant",
         ]
 
-    def create(self, validated_data) -> BankAccount:
+    def create(self, validated_data) -> CreditCard:
         initial_balance = validated_data.pop("initial_balance")
         account = Account.objects.create(
             name=validated_data["name"],
             balance=initial_balance,
             tenant=validated_data["tenant"],
         )
-        new_instance = BankAccount.objects.create(account=account, **validated_data)
+        new_instance = CreditCard.objects.create(account=account, **validated_data)
         return new_instance
 
-    def to_representation(self, instance: BankAccount) -> dict:
-        return BankAccountSerializer(instance).data
+    def to_representation(self, instance: CreditCard) -> dict:
+        return CreditCardSerializer(instance).data
 
 
-class BankAccountViewSet(TenantModelViewSet[BankAccount]):
-    queryset = BankAccount.objects.all()
-    serializer_class = BankAccountSerializer
+class CreditCardViewSet(TenantModelViewSet[CreditCard]):
+    queryset = CreditCard.objects.all()
+    serializer_class = CreditCardSerializer
     permission_classes = [permissions.DjangoModelPermissions]
     lookup_field = "uuid"
 
@@ -93,10 +102,10 @@ class BankAccountViewSet(TenantModelViewSet[BankAccount]):
 
     def get_serializer_class(self):
         if self.action == "create":
-            return CreateBankAccountSerializer
+            return CreateCreditCardSerializer
         if self.action in ("update", "partial_update"):
-            return UpdateBankAccountSerializer
-        return BankAccountSerializer
+            return UpdateCreditCardSerializer
+        return CreditCardSerializer
 
-    def perform_destroy(self, instance: BankAccount) -> None:
+    def perform_destroy(self, instance: CreditCard) -> None:
         instance.soft_delete()
